500bf2701471f3c9b52986867583a46e1c03df44
NicoMdz <nick.mdz.b@gmail.com>
2024-09-01 16:44:47 -0600

    Cambios

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,8 +1,17 @@
+import AddProductsForm from "@/components/products/AddProductsForm";
+import ProductForm from "@/components/products/ProductForm";
+import Heading from "@/components/ui/Heading";
 
 
 export default function ProductsPage() {
 
   return (
-    <h1>ProductsPage</h1>
+    <>
+      <Heading>Nuevo Producto</Heading>
+
+      <AddProductsForm>
+        <ProductForm />
+      </AddProductsForm>
+    </>
   )
 }
